8d74ad43290b49e0b66aa9661fe07ce74d68507d
ohanqo <gm.anqo@gmail.com>
2018-03-12 11:59:42 +0100

    Modif rest-server.py

--- server/myapp/rest-server.py
+++ server/myapp/rest-server.py
@@ -33,23 +33,6 @@ def analyze_text():
     analyze = Sentiment(request.args.get("text"))
     return jsonify({'sentiment': analyze.analyze_text()})
 
-@app.route('/tweets/getTweet', methods=['GET'])
-@cross_origin()
-def get_tweet():
-
-    # je crée seulement un tweet comme exemple
-    myTweet = Tweet()
-
-    # j'ai analysé son sentiment
-    myTweet.sentiment_analyze_tweet()
-
-    # Je l'ai inclus dans une liste
-    tweets = []
-    tweets.append(myTweet.serialize())
-
-    #je donne la réponse au serveur
-    return jsonify({'tweets': tweets})
-
 @app.route('/statistics/hashtag/<hashtagSearched>')
 @cross_origin()
 def getStatistics(hashtagSearched):
