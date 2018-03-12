1be72f1ca0438c7581be22cf28cb502b65c340fc
Your Name <you@example.com>
2018-03-12 11:14:22 +0100

    ajout de dashboard barchart

---
+++ website/src/components/BarChart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+import '../style/css/map.css';
+
+class BarChart extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default BarChart;
