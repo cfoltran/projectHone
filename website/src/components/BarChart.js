a040007cb5e33437c4b789562a1fdcabf2ecba81
Your Name <you@example.com>
2018-03-13 17:46:31 +0100

    .

--- website/src/components/BarChart.js
+++ website/src/components/BarChart.js
@@ -6,6 +6,54 @@ class BarChart extends Component {
   render() {
     return (
 
+      // set the dimensions and margins of the graph
+      var margin = {top: 20, right: 20, bottom: 30, left: 40},
+          width = 960 - margin.left - margin.right,
+          height = 500 - margin.top - margin.bottom
+
+      // set the ranges
+      var x = d3.scaleBand()
+                .range([0, width])
+                .padding(0.1)
+      var y = d3.scaleLinear()
+                .range([height, 0])
+
+      // append the svg object to the body of the page
+      // append a 'group' element to 'svg'
+      // moves the 'group' element to the top left margin
+      var svg = d3.select("body").append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")")
+
+      // get the data
+      d3.json("tweet.json", function(error, data) {
+        if (error) throw error
+
+        // Scale the range of the data in the domains
+        x.domain(data.map(function(d) { return d.name }));
+        y.domain([0, d3.max(data, function(d) { return d.tweet })])
+
+        // append the rectangles for the bar chart
+        svg.selectAll(".bar")
+            .data(data)
+          .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.name) })
+            .attr("width", x.bandwidth())
+            .attr("y", function(d) { return y(d.tweet) })
+            .attr("height", function(d) { return height - y(d.tweet) })
+
+        // add the x Axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
+
+        // add the y Axis
+        svg.append("g")
+            .call(d3.axisLeft(y))
     );
   }
 }
--- website/src/components/DashBoard.js
+++ website/src/components/DashBoard.js
@@ -3,9 +3,59 @@ import * as d3 from 'd3';
 import '../style/css/map.css';
 
 class DashBoard extends Component {
+
+    initDashboard(){
+         var $ppc = $('.progress-pie-chart'),
+        percent = parseInt($ppc.data('percent')),
+        deg = 360*percent/100;
+      if (percent > 50) {
+        $ppc.addClass('gt-50');
+      }
+      d3.select('.ppc-progress-fill').style('transform','rotate('+ deg +'deg)');
+      d3.select('.ppc-percents span').html(percent+'%');
+
+        d3.json("data.json", function(data) {
+          console.log(data[0]);
+          d3.select('#rt').html(data[0].age)
+          d3.select('#like').html(data[0].age)
+          d3.select('#man').html(data[0].age)
+          d3.select('#woman').html(data[0].age)
+    });
+    }
   render() {
     return (
-        
+           <div class="progressDiv">
+            <div class="statChartHolder">
+                <div class="progress-pie-chart" data-percent="43"><!--Pie Chart -->
+                    <div class="ppc-progress">
+                        <div class="ppc-progress-fill"></div>
+                    </div>
+                    <div class="ppc-percents">
+                    <div class="pcc-percents-wrapper">
+                        <span>%</span>
+                    </div>
+                    </div>
+                </div><!--End Chart -->
+            </div>
+            <div class="statRightHolder">
+
+
+                <ul>
+                <li> <h3 id='rt' class="blue"></h3> <span>Retweets</span></li>
+                <li> <h3 id='like' class="purple"></h3> <span>Like</span></li>
+                </ul>
+
+                    <ul class="statsLeft">
+                    <li><h3 id='man'class="yellow"></h3> <span>Homme</span></li>
+                    <li><h3 id='woman'class="red"></h3> <span>Femme</span></li>
+                    </ul>
+                        <ul class="statsRight">
+                            <li><h3>18%</h3> <span>Région</span></li>
+                            <li><h3>23%</h3> <span>Goals</span></li>
+                        </ul>
+            </div>
+
+        </div>
     );
   }
 }
