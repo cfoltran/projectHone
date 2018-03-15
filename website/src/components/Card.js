efef1f105b8ef6326c6f0827020aaec000e083c5
ohanqo <gm.anqo@gmail.com>
2018-03-15 09:13:22 +0100

    maj de la branche

---
+++ website/src/components/Card.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Card } from 'reactstrap';
+
+export default class Card extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
