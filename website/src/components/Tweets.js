d55b92cf39221f3329cc36a067dfa9e5da335dbc
alex-matias <alex.matias@u-psud.fr>
2018-03-22 09:35:12 +0100

    Changement des couleurs du text avec la page

--- website/src/components/Tweets.js
+++ website/src/components/Tweets.js
@@ -26,13 +26,15 @@ export default class Tweets extends React.Component {
 
     render() {
         let styleBg=(this.props.checked)?"bg-dark":"bg-light";
+        let styleTitre=(this.props.checked)?"font-70 text-light":"font-70 text-dark";
+        let styleText=(this.props.checked)?"font-40 text-light":"font-40 text-dark";
         return (
             <div>
                 <section className={styleBg} id="home">
                     <div className="container padding-150" >
                         <div className="row text-center">
                             <div className="col-md-12" id="tweets">
-                                <h1 className="font-70 text-dark">Tweets </h1><br/>
+                                <h1 className={styleTitre}>Tweets </h1><br/>
                                 <main>
                                     {/*
                                         this.state.tweets.map((tweet, i) => {
@@ -57,9 +59,9 @@ export default class Tweets extends React.Component {
                     <div className="container padding-150">
                         <div className="row text-center">
                             <div className="col-md-12">
-                                <h1 className="font-70 text-dark">Presentation<br/></h1>
+                                <h1 className={styleTitre}>Presentation<br/></h1>
                                 <hr/><br/>
-                                    <p className="font-40 text-cloud text-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab aliquid consectetur cumque,
+                                    <p className={styleText}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab aliquid consectetur cumque,
                                         deleniti doloremque ex expedita fugiat labore laborum mollitia officia, perferendis porro quod sapiente sed sequi soluta ut!</p>
                             </div>
                         </div>
