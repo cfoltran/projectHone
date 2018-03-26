5db6aa035922526ebfd52be0410e0c730a72e46b
alex-matias <alex.matias@u-psud.fr>
2018-03-26 12:11:19 +0200

    Fix erreurs

--- website/src/components/Searchbar.js
+++ website/src/components/Searchbar.js
@@ -95,7 +95,7 @@ class Searchbar extends Component
                         {/*                 <!--Modal-->                     */}
                         {/*<!--===========================================-->*/}
                         <ul className="navbar-nav ml-auto">
-                            {/*<!-- Bouton switch -->*/}
+                            {/*<!-- Switch button -->*/}
                             <Switch checked={this.props.checked} onSwitchHome={this.props.onSwitchHome}/>
                             <li className="nav-item page-scroll">
                                 <Button color="primary" onClick={this.toggle}> <i className="fas fa-search"> Rechercher</i></Button>
