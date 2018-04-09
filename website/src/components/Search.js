45b8cb6adceea4ed32d018dfb811765d155ca281
Owalid <othmane.el.ayadi@gmail.com>
2018-04-09 23:12:41 +0200

    modif affichage d3

--- website/src/components/Search.js
+++ website/src/components/Search.js
@@ -1,10 +1,9 @@
 //todo collect input value from searchBar.js
 import React from 'react';
 
-import NavbarFeatures from './NavbarFeatures';
-import DashBoard from "./DashBoard";
+import Searchbar from './Searchbar';
+import DashBoard from "./DashBoard"
 import BarChart from './BarChart';
-import FooterPage from "./Footer";
 
 
 class Search extends React.Component{
@@ -15,18 +14,18 @@ class Search extends React.Component{
         return (
             <div>
 
-                <NavbarFeatures/>
+                <Searchbar/>
                 {/*--------------------------------*/}
                 {/*----------Get the search--------*/}
                 {/*--------------------------------*/}
-                <section className="bg-dark text-center">
+                <section className="bg-white text-center">
                     <div className="container">
-                        <h2 className="font-60">Recherche:</h2>
+                        <h2 className="font-70 text-dark">Recherche:</h2>
                         <div className="row text-center">
                             <div className="col-md-12  padding-150">
-                                 <p className="text-cloud"> Vous avez recherchez: {this.props.match.params.tag} <br/>
-                        Avec le sentiment: {this.props.match.params.sementic}
-                        {/*Todo get the sementic*/}
+                                <p className="text-dark font-40"> Vous avez recherchez: {this.props.match.params.tag} <br/>
+                                    Avec le sentiment: {this.props.match.params.sementic}
+                                    {/*Todo get the sementic*/}
                                 </p>
                             </div>
 
@@ -37,11 +36,11 @@ class Search extends React.Component{
                 {/*--------------------------------*/}
                 {/*----------Dash Board------------*/}
                 {/*--------------------------------*/}
-                <section>
+                <section className="bg-dark">
                     <div className="container">
-                        <h2 className="font-60 text-center">Dash Board :</h2>
+                        <h2 className="font-70 text-center text-cloud">Dash Board :</h2>
                         <div className="row">
-                    <DashBoard/>
+                            <DashBoard/>
                         </div>
                     </div>
                 </section>
@@ -49,18 +48,17 @@ class Search extends React.Component{
                 {/*--------------------------------*/}
                 {/*-----------Bar Chart------------*/}
                 {/*--------------------------------*/}
-                <section className="bg-white">
-                    <div className="container">
-                        <h2 className="font-60 text-center padding-150">Bar chart :</h2>
+                <div>
+                    <section className="bg-white">
+                        <div className="container">
+                            <h2 className="font-70 text-center padding-10">Bar chart:</h2>
                             <div className="row">
-                            <BarChart/>
+                                <BarChart/>
                             </div>
-                    </div>
-                </section>
-                <FooterPage/>
+                        </div>
+                    </section >
+                </div>
             </div>
-
-
         )
     }
 
