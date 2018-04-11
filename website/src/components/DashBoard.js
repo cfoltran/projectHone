e8a050e84f0392e686d5893b0e183d26ce0cd71a
Trung Toan Banh Nguyen <trung-toan.banh-nguyen@h04.sysres.iut-orsay.fr>
2018-04-11 10:50:52 +0200

    Dashboard

--- website/src/components/DashBoard.js
+++ website/src/components/DashBoard.js
@@ -24,10 +24,10 @@ class DashBoard extends Component {
 
             d3.json("/static/dataForDashBoard.json", function(data) {
               console.log(data[0]);
-              d3.select('#rt').html(data[0].age);
-              d3.select('#like').html(data[0].age);
-              d3.select('#man').html(data[0].age);
-              d3.select('#woman').html(data[0].age)
+              d3.select('#tweet').html(data[0].age);
+              d3.select('#rt').html(data[1].age);
+ 				d3.select('#humeur').html(data[1].age);
+            
         });
     }
   render() {
@@ -35,13 +35,13 @@ class DashBoard extends Component {
            <div className="progressDiv">
             <div className="statChartHolder">
                 {/*Pie Chart*/}
-                <div className="progress-pie-chart" data-percent="43">
+                <div className="progress-pie-chart" data-percent="90">
                     <div className="ppc-progress">
                         <div className="ppc-progress-fill"></div>
                     </div>
                     <div className="ppc-percents">
                     <div className="pcc-percents-wrapper">
-                        <span>%</span>
+                        <span> 4%</span>
                     </div>
                     </div>
                 </div>
@@ -50,19 +50,13 @@ class DashBoard extends Component {
             <div className="statRightHolder">
 
                 <ul>
-                    <li> <h3><span>Retweets</span></h3> <div id='rt' className="font-30"/>
+                    <li> <h3><span>Nombre de Tweet</span></h3> <div id='tweet' className="font-30"/>
                   </li>
-                    <li> <h3><span>Like</span></h3> <div id='like' className="font-30"/> </li>
+                    <li> <h3><span>Nombre de RT moyen</span></h3> <div id='rt' className="font-30"/> </li>
+					<li> <h3><span>Humeur</span></h3> <div id='humeur' className="font-30"/> </li>
                 </ul>
 
-                    <ul className="statsLeft">
-                    <li><div id='man' className=""/> <span>Homme</span></li>
-                    <li><div id='woman' className=""/> <span>Femme</span></li>
-                    </ul>
-                        <ul className="statsRight">
-                            <li><h3>18%</h3> <span>Région</span></li>
-                            <li><h3>23%</h3> <span>Goals</span></li>
-                        </ul>
+                    
             </div>
 
         </div>
@@ -71,3 +65,4 @@ class DashBoard extends Component {
 }
 
 export default DashBoard;
+
