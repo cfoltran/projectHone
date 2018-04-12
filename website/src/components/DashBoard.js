057d882480a024e393bf410e6fecaf9ca71889e7
Toananas123 <33756462+Toananas123@users.noreply.github.com>
2018-04-12 12:05:42 +0200

    Update DashBoard.js

--- website/src/components/DashBoard.js
+++ website/src/components/DashBoard.js
@@ -1,67 +1,65 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 import '../style/css/DashBoard.css';
-
 class DashBoard extends Component {
-
-    componentDidMount() {
-        this.initDashboard()
-    }
-
-    componentDidUpdate(){
-        this.initDashboard()
-    }
-
-    initDashboard(){
-            var ppc = d3.select('.progress-pie-chart'),
-            percent = parseInt(ppc.data('percent'),10),
-            deg = 360*percent/100;
-          if (percent > 50) {
-            ppc.addClass('gt-50');
-          }
-          d3.select('.ppc-progress-fill').style('transform','rotate('+ deg +'deg)');
-          d3.select('.ppc-percents span').html(percent+'%');
-
-            d3.json("localhost:5001/statistics/hashtag", function(data) {
-              console.log(data[0]);
-              d3.select('#tweet').html(data[0].age);
-              d3.select('#rt').html(data[1].age);
- 				d3.select('#humeur').html(data[1].age);
-            
-        });
-    }
-  render() {
-    return (
-           <div className="progressDiv">
-            <div className="statChartHolder">
-                {/*Pie Chart*/}
-                <div className="progress-pie-chart" data-percent="90">
-                    <div className="ppc-progress">
-                        <div className="ppc-progress-fill"></div>
-                    </div>
-                    <div className="ppc-percents">
-                    <div className="pcc-percents-wrapper">
-                        <span> 4%</span>
-                    </div>
-                    </div>
-                </div>
-                {/*End Chart*/}
-            </div>
-            <div className="statRightHolder">
-
-                <ul>
-                    <li> <h3><span>Tweets</span></h3> <div id='tweet' className="font-30"/>
-                  </li>
-                    <li> <h3><span>Retweet moyen</span></h3> <div id='rt' className="font-30"/> </li>
-					<li> <h3><span>Humeur</span></h3> <div id='humeur' className="font-30"/> </li>
-                </ul>
-
-                    
-            </div>
-
-        </div>
-    );
-  }
+constructor(props) {
+super(props);
+this.state = {
+tag: this.props,
+};
+//get the hash do this.state.tag
 }
+componentDidMount() {
+this.initDashboard(this.state.tag)
+}
+componentDidUpdate(){
+this.initDashboard(this.state.tag)
+}
+initDashboard(tag){
+var ppc = d3.select('.progress-pie-chart'),
+percent = parseInt(ppc.data('percent'),10),
+deg = 360*percent/100;
+if (percent > 50) {
+ppc.addClass('gt-50');
+}
+d3.select('.ppc-progress-fill').style('transform','rotate('+ deg +'deg)');
+d3.select('.ppc-percents span').html(percent+'%');
+d3.json("http://localhost:5001/statistics/hashtag/"+tag, function(data) {
 
+d3.select('#tweet').html(data.statistics['0'].NumbersOfTweets);
+d3.select('#rt').html(data.statistics['0'].AverageRetweets);
+d3.select('#humeur').html(data.statistics['0'].AveragePolarity);
+
+});
+}
+render() {
+return (
+<div className="progressDiv">
+<div className="statChartHolder">
+{/*Pie Chart*/}
+<div className="progress-pie-chart" >
+<div className="ppc-progress">
+<div className="ppc-progress-fill"></div>
+</div>
+<div className="ppc-percents">
+<div className="pcc-percents-wrapper">
+<span> %</span>
+</div>
+</div>
+</div>
+{/*End Chart*/}
+</div>
+<div className="statRightHolder">
+<ul>
+<li> <h3><span>Nombre de Tweet</span></h3> <div id='tweet' className="font-30"/>
+</li>
+<li> <h3><span>Nombre de RT moyen</span></h3> <div id='rt' className="font-30"/> </li>
+<li> <h3><span>Humeur</span></h3> <div id='humeur' className="font-30"/> </li>
+</ul>
+
+</div>
+</div>
+);
+}
+}
 export default DashBoard;
